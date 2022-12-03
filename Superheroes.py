4a1e986565e83ed6283f033ce1ea4a38c54c8fca
Nikolai Seliverstov <seliverstov.nd@phystech.edu>
2022-12-03 17:19:16 +0300

    FInal version without TOKEN

--- Superheroes.py
+++ Superheroes.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
-url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api'
-response = requests.get(url + '/all.json')
-heroes = json.loads(response.text)
-heroes_intelligence = {'Hulk': 0, 'Captain America': 0, 'Thanos': 0}
-for hero in heroes:
-    if hero['name'] in heroes_intelligence.keys():
-        heroes_intelligence[hero['name']] = hero['powerstats']['intelligence']
-print(max(heroes_intelligence, key=lambda key: heroes_intelligence[key]))
+if __name__ == '__main__':
+    url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api'
+    response = requests.get(url + '/all.json')
+    heroes = json.loads(response.text)
+    heroes_intelligence = {'Hulk': 0, 'Captain America': 0, 'Thanos': 0}
+    for hero in heroes:
+        if hero['name'] in heroes_intelligence.keys():
+            heroes_intelligence[hero['name']] = hero['powerstats']['intelligence']
+    print(max(heroes_intelligence, key=lambda key: heroes_intelligence[key]))
 
---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = ''
+script_dir = os.getcwd()
+file_dir = os.path.join(script_dir, 'file_to_YA')
+path_to_file = os.path.join(file_dir, 'foto_football.jpg')
+
+
